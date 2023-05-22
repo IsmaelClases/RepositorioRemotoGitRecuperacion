0da1615c58737e6f1a12a6709f1c4e480752f67e
DAM1 <DAM1@192.168.7.137>
2023-05-22 17:41:04 +0200

    nueva rama guardando

--- ProyectoGitRecuperacion/src/paqueteGitRecuperacion/GitRecuperacion.java
+++ ProyectoGitRecuperacion/src/paqueteGitRecuperacion/GitRecuperacion.java
@@ -5,6 +5,8 @@ public class GitRecuperacion {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Mensaje");
+		System.out.println("Mensaje 2");
+		System.out.println("Mensaje 3");
 	}
 
 }
