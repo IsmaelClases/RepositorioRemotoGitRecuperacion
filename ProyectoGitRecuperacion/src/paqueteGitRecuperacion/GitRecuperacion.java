8ca4e68532dc7212ded1c9b53a427d0985a3fd1d
DAM1 <DAM1@192.168.7.137>
2023-05-22 17:35:37 +0200

    Confirmar Cambios

---
+++ ProyectoGitRecuperacion/src/paqueteGitRecuperacion/GitRecuperacion.java
@@ -0,0 +1,10 @@
+package paqueteGitRecuperacion;
+
+public class GitRecuperacion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Mensaje");
+	}
+
+}
